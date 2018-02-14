ebfe24ed59f78c00c3b873d29c8bdf2fd4f57717
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 13:35:11 -0700

    postLocation action

--- src/actions/locationActions.js
+++ src/actions/locationActions.js
@@ -1,25 +1,43 @@
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
-
-const storeAllLocations = (locations) => {
+const storeAllLocations = locations => {
   return {
-    type: 'STORE_LOCATIONS',
-    data: locations.locations,
+    type: "STORE_LOCATIONS",
+    data: locations.locations
+  };
+};
+
+const storeNewLocation = location => {
+  return {
+    type: "STORE_LOCATION",
+    data: location
   };
 };
 
 const fetchAllLocations = () => {
-  return (dispatch) => {
-    return fetch('/locations', {
+  return dispatch => {
+    return fetch("/locations", {
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
     })
       .then(locations => locations.json())
       .then(json => dispatch(storeAllLocations(json)));
   };
 };
 
+const postLocation = location => {
+  return async dispatch => {
+    const fetcher = await fetch("locations", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(location)
+    });
+    const json = await fetcher.json();
+    dispatch(storeNewLocation(json));
+    fetchAllLocations();
+  };
+};
 
-export { fetchAllLocations }
+export { fetchAllLocations, postLocation };
