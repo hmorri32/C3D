b1782695312801255afda43ebd777905824397dd
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 12:35:36 -0700

    rudimentary lint check on server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const bodyParser = require('body-parser');
-const path = require('path')
+const bodyParser = require("body-parser");
+const path = require("path");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -10,39 +10,40 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // Get all users
 const initialLocations = [
   {
-    id: 'id1',
-    name: 'Denver',
+    id: "id1",
+    name: "Denver",
     lat: 39.742043,
-    lng: -104.991531,
+    lng: -104.991531
   },
   {
-    id: 'id2',
-    name: 'LA',
+    id: "id2",
+    name: "LA",
     lat: 34.052235,
-    lng: -118.243683,
+    lng: -118.243683
   },
   {
-    id: 'id3',
-    name: 'Boston',
+    id: "id3",
+    name: "Boston",
     lat: 42.364506,
-    lng: -71.038887,
-  },
+    lng: -71.038887
+  }
 ];
 
 app.locals.idIndex = 3;
 app.locals.locations = initialLocations;
 
-app.get('/locations', (request, response) => response.send({ locations: app.locals.locations }));
+app.get("/locations", (request, response) =>
+  response.send({ locations: app.locals.locations })
+);
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
 
 const portNumber = process.env.PORT || 3001;
 
-app.listen(portNumber, () => {
-  console.log('RrrarrrrRrrrr server alive on port 3001');
-});
+/* eslint-disable no-console */
+app.listen(portNumber, () => console.log("RrrarrrrRrrrr server alive on port 3001"));
