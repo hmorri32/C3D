b987d5d0578485dec52d606397760be71e3c09ed
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 16:16:53 -0700

    creates destroy poly line reducer

--- src/reducers/PolygonReducer.js
+++ src/reducers/PolygonReducer.js
@@ -1,17 +1,23 @@
 const PolyLines = (state = { data: [] }, action) => {
   switch (action.type) {
-  case "CREATE_POLY_LINE":
-    return Object.assign({}, state, {
-      data: state.data.concat([action.data])
-    });
+    case "CREATE_POLY_LINE":
+      return Object.assign({}, state, {
+        data: state.data.concat([action.data])
+      });
 
-  case "DESTROY_POLY_LINE":
-    // console.log('suh');
-    break;
-
-  default:
-    return state;
+    case "DESTROY_POLY_LINE":
+      return Object.assign({}, state, {
+        data: filtered(state.data, action.data)
+      });
+    default:
+      return state;
   }
+};
+
+const filtered = (polyLines, latlng) => {
+  return polyLines.filter(
+    coord => JSON.stringify(coord) !== JSON.stringify(latlng)
+  );
 };
 
 export default PolyLines;
