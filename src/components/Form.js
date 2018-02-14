9c6b034e2d32a7281a834118eff35a4fcfd6a731
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 14:18:08 -0700

    validates lat lng and name

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,50 +9,87 @@ class Form extends Component {
   }
 
   submitForm(e, data) {
-    const { lat, lng, name } = data;
     const { postLocation } = this.props;
     e.preventDefault();
+    if (data.name && this.checkCoordinates(data)) {
+      postLocation(data);
+      this.resetInputs(this);
+      this.clearErrors();
+    } else {
+      this.setError();
+    }
+  }
 
-    postLocation(data);
+  setError() {
+    this.setState({
+      formErrors: "Please enter valid latitude and longitude coordinates."
+    });
+  }
+
+  clearErrors() {
+    this.setState({
+      formErrors: ""
+    });
+  }
+
+  checkCoordinates(data) {
+    return (
+      parseFloat(data.lng) && Math.abs(data.lng) <= 180 && (parseFloat(data.lat) && Math.abs(data.lat) <= 90)
+    );
+  }
+
+  resetInputs(form) {
+    form.name.value = "";
+    form.lat.value = "";
+    form.lng.value = "";
   }
 
   render() {
+    const { formErrors } = this.state;
     return (
       <form className="form">
         <label>
           Name
           <input
-            ref={(input) => { this.name = input; }}
+            ref={input => {
+              this.name = input;
+            }}
             type="text"
           />
         </label>
         <label>
           Lat
           <input
-            ref={(input) => { this.lat = input; }}
+            ref={input => {
+              this.lat = input;
+            }}
             type="text"
           />
         </label>
         <label>
           Lon
           <input
-            ref={(input) => { this.lng = input; }}
-            type="text"/>
+            ref={input => {
+              this.lng = input;
+            }}
+            type="text"
+          />
         </label>
         <button
           type="submit"
-          onClick={(e) => this.submitForm(e, {
-            name: this.name.value,
-            lat: this.lat.value,
-            lng: this.lng.value
-          })}
-        >
-            Save
+          onClick={e =>
+            this.submitForm(e, {
+              name: this.name.value,
+              lat: this.lat.value,
+              lng: this.lng.value
+            })
+          }>
+          Save
         </button>
+        {formErrors ? <p>{formErrors}</p> : ""}
       </form>
     );
   }
 }
-
 
 export default Form;
