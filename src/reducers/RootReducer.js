efa45b44d5c313915627757f78ebd72ce34f96f5
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 16:02:39 -0700

    adds polygon reducer to root reducer

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux';
-import Locations from './LocationsReducer';
+import { combineReducers } from "redux";
+import Locations from "./LocationsReducer";
+import PolyLines from "./PolygonReducer";
 
 const RootReducer = combineReducers({
   Locations,
+  PolyLines
 });
 
 export default RootReducer;
