e6e7ff7f61888fbe878281ed8782cd54e371bc4f
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 13:46:16 -0700

    formats allmarkers container

--- src/containers/AllMarkers.js
+++ src/containers/AllMarkers.js
@@ -1,14 +1,14 @@
 /*eslint-disable no-unused-vars*/
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import AllMarkers from '../components/AllMarkers'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import AllMarkers from "../components/AllMarkers";
 
-const mapStateToProps = (state) => {
-  return { locations: state.Locations.data }
-}
+const mapStateToProps = state => {
+  return { locations: state.Locations.data };
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {}
-}
+const mapDispatchToProps = dispatch => {
+  return {};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllMarkers);
