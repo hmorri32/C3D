5a935bad6729f1de06a098e71d0d78cd195f3c6b
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 14:46:54 -0700

    pan to new center on post

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -6,16 +6,34 @@ class LeafletMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      center: [39.750809, -104.99681],
+      newCenter: []
     };
   }
 
+  componentWillReceiveProps(next) {
+    if (this.props.locations.length) {
+      const { locations } = next;
+      const { length } = locations;
+      const recentlyPosted = locations[length - 1];
+      this.panToNewLocation(recentlyPosted);
+    }
+  }
+
+  panToNewLocation(location) {
+    this.setState({
+      newCenter: [location.lat, location.lng]
+    });
+  }
+
   render() {
+    const { center, newCenter } = this.state;
     return (
       <div className="map-container">
         <Map
           className="map"
           zoomControl={false}
-          center={[39.750809, -104.99681]}
+          center={newCenter.length ? newCenter : center}
           zoom={4}
           maxBounds={[[85, 100], [-85, -280]]}>
           <TileLayer
