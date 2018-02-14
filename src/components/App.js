ff0b6f5a8fb55118a8739c9e7392e36d23f99f05
Hugh Morrison <hmorri32@gmail.com>
2018-02-14 13:35:21 -0700

    lints app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import LeafletMap from '../containers/LeafletMap';
-import FormContainer from '../containers/FormContainer';
+import React, { Component } from "react";
+import LeafletMap from "../containers/LeafletMap";
+import FormContainer from "../containers/FormContainer";
 
 class App extends Component {
   componentDidMount() {
     this.getLocations();
   }
+
   getLocations() {
     this.props.fetchAllLocations();
   }
+
   render() {
+    const { locations } = this.props;
     return (
       <div className="App">
         <FormContainer />
-        <LeafletMap locations={this.props.locations} />
+        <LeafletMap locations={locations} />
       </div>
     );
   }
