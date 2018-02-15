b509e1ec9ecf229d9d29e9ff948731d45fb81e8c
Hugh Morrison <hmorri32@gmail.com>
2018-02-15 06:24:50 -0700

    rename case in location reducer to STORE

--- src/reducers/LocationsReducer.js
+++ src/reducers/LocationsReducer.js
@@ -10,7 +10,7 @@ const Locations = (state = initialState, action) => {
         data: state.data.concat(action.data)
       });
 
-    case "POST_LOCATION":
+    case "STORE_LOCATION":
       return Object.assign({}, state, {
         data: [...state.data, action.data]
       });
