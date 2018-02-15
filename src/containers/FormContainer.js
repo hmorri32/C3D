147966caa1f9bdf0f419f9dda128e1aef280667f
Hugh Morrison <hmorri32@gmail.com>
2018-02-15 06:24:41 -0700

    mapdispatch, storenewlocation

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,14 +1,14 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import Form from "../components/Form";
-import { postLocation, fetchAllLocations } from "../actions/locationActions";
+import { postLocation, storeNewLocation } from "../actions/locationActions";
 
 const mapStateToProps = state => {
   return {};
 };
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ postLocation, fetchAllLocations }, dispatch);
+  return bindActionCreators({ postLocation, storeNewLocation }, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form);
